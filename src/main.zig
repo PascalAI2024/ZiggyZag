9edd66053607a6ec528321fe818aa3fb079ca429
PascalAI2024 <159666353+PascalAI2024@users.noreply.github.com>
2026-05-14 14:55:45 -0400

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
+    var stdin_buffer: [4096]u8 = undefined;
+    var stdin = std.Io.File.stdin().readerStreaming(init.io, &stdin_buffer);
     var stdout = std.Io.File.stdout().writer(init.io, &.{});
 
     try stdout.interface.print("$ ", .{});
+
+    const command = try stdin.interface.takeDelimiter('\n');
+    const command_text = std.mem.trimEnd(u8, command.?, "\r");
+    try stdout.interface.print("{s}: command not found\n", .{command_text});
 }
